21ddf83166db8a321b61004e3a1c04b36d5bae32
student apna college <modassir.ece.21@nitap.ac.in>
2024-10-28 11:39:56 +0530

    nutritioncal

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React  from 'react'
+import './App.css'
+import NutritionMeter from './NutritionMeter'
+
+function App() {
+
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <NutritionMeter/>
+      </div>
+  )
+}
+
+export default App
